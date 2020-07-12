bd3652fa6af5023df8ae309a97ffe27fdc629658
SPIDERBHAI <playboycracker.official@gmail.com>
2020-07-12 19:23:06 +0530

    Upload HunterGang UserBot Files

---
+++ userbot/plugins/alive.py
@@ -0,0 +1,25 @@
+"""Check if userbot alive. If you change these, you become the gayest gay such that even the gay world will disown you."""
+import asyncio
+from telethon import events
+from uniborg.util import admin_cmd
+from userbot import ALIVE_NAME
+from telethon.tl.types import ChannelParticipantsAdmins
+DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "Unknown"
+pm_caption = "`HUNTERGANG IS:` **ONLINE**\n\n"
+pm_caption += "**SYSTEM STATUS**\n"
+pm_caption += "`TELETHON VERSION:` **6.0.9**\n`Python:` **3.7.4**\n"
+pm_caption += "`DATABASE STATUS:` **Functional**\n"
+pm_caption += "**Current Branch** : `master`\n"
+pm_caption += f"**My Boss** : {DEFAULTUSER} \n"
+pm_caption += "**Heroku Database** : `AWS - Working Properly`\n\n"
+pm_caption += "Copyright : By [SpiderBhai@Github](GitHub.com/SpiderBhai)\n"
+pm_caption += " [Deploy HunterGang UserBot](https://github.com/SPIDERBHAI/HunterGang)"
+#@command(outgoing=True, pattern="^.alive$")
+@borg.on(admin_cmd(pattern=r"alive"))
+async def amireallyalive(alive):
+    chat = await alive.get_chat()
+    await alive.delete()
+    """ For .alive command, check if the bot is running.  """
+    await borg.send_message(chat, FRIDAY_IS_ALIVE) 
+    await alive.delete() 
+    await borg.send_file(alive.chat_id, PM_IMG,caption=pm_caption)
