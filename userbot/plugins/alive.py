880848e37567e42c2b526a46ddbb0ad8d69e2ce8
SPIDER_XD <66294171+SPIDERBHAI@users.noreply.github.com>
2020-07-12 22:13:25 +0530

    Update alive.py

--- userbot/plugins/alive.py
+++ userbot/plugins/alive.py
@@ -1,25 +1,18 @@
 """Check if userbot alive. If you change these, you become the gayest gay such that even the gay world will disown you."""
 import asyncio
 from telethon import events
-from uniborg.util import admin_cmd
-from userbot import ALIVE_NAME
 from telethon.tl.types import ChannelParticipantsAdmins
-DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "Unknown"
-pm_caption = "`HUNTERGANG IS:` **ONLINE**\n\n"
-pm_caption += "**SYSTEM STATUS**\n"
-pm_caption += "`TELETHON VERSION:` **6.0.9**\n`Python:` **3.7.4**\n"
-pm_caption += "`DATABASE STATUS:` **Functional**\n"
-pm_caption += "**Current Branch** : `master`\n"
-pm_caption += f"**My Boss** : {DEFAULTUSER} \n"
-pm_caption += "**Heroku Database** : `AWS - Working Properly`\n\n"
-pm_caption += "Copyright : By [SpiderBhai@Github](GitHub.com/SpiderBhai)\n"
-pm_caption += " [Deploy HunterGang UserBot](https://github.com/SPIDERBHAI/HunterGang)"
-#@command(outgoing=True, pattern="^.alive$")
-@borg.on(admin_cmd(pattern=r"alive"))
-async def amireallyalive(alive):
-    chat = await alive.get_chat()
-    await alive.delete()
-    """ For .alive command, check if the bot is running.  """
-    await borg.send_message(chat, FRIDAY_IS_ALIVE) 
-    await alive.delete() 
-    await borg.send_file(alive.chat_id, PM_IMG,caption=pm_caption)
+from platform import uname
+from userbot import ALIVE_NAME
+from userbot.utils import admin_cmd
+
+
+DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "Set ALIVE_NAME in config vars in Heroku"
+INDIANBOT_IS_ALIVE = ("**I'm Alive^.^** \n`BOT Status : ` **☣Hot**\n\n"
+                     f"`My peru owner`: {DEFAULTUSER}\n\n"
+                     "`Indian Bot Version:` 1.0\n`Python:` **3.7.4**\n"
+                     "`Database Status:` **😀ALL OK**\n\n`Always with you, my master!\n`"
+                     "**Bot Creator:** [MrSpider](t.me/MrSpider_Genuine)\n\n"
+                     "     [Deploy This HunterGang UserBot](https://github.com/SPIDERBHAI/HunterGang)") 
+
+
